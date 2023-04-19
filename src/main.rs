17e7d766164ab2c105a1c8a7c2b7836abda826c0
lalala-233 <2317987274@qq.com>
2023-04-19 17:06:18 +0800

    feat: 猜数字游戏

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("这是一个猜数字游戏，程序会生成一个随机数。");
+    let mut time = 0;
+    let random = rand::thread_rng().gen_range(1..=100);
+    loop {
+        println!("\n这是你第 {time} 次猜测，你猜的数字为（范围：1~100）：");
+        time += 1;
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("错误：无法获取输入！");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(guess) => guess,
+            Err(_) => {
+                println!("错误：请输入数字！");
+                time -= 1;
+                continue;
+            }
+        };
+        match guess.cmp(&random) {
+            Ordering::Equal => {
+                println!("你猜对了！");
+                break;
+            }
+            Ordering::Greater => println!("你猜的数大了～"),
+            Ordering::Less => println!("你猜的数小了～"),
+        }
+    }
+}
