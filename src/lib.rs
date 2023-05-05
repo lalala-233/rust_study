48466ad0c8f4d6ae161858c09a4df131f48433a2
lalala-233 <2317987274@qq.com>
2023-05-05 13:34:23 +0800

    feat: Map 类型

--- src/lib.rs
+++ src/lib.rs
@@ -1,92 +1,36 @@
-use std::{env, error::Error, fs, process};
-pub struct Config {
-    query: String,
-    path: String,
-    ignore_case: bool,
+struct Position {
+    x: usize,
+    y: usize,
 }
-impl Config {
-    pub fn build(args: &[String]) -> Self {
-        Config::new(args).unwrap_or_else(|err| {
-            eprintln!("参数错误：{err}");
-            process::exit(1);
-        })
-    }
-
-    fn new(args: &[String]) -> Result<Self, &'static str> {
-        if args.len() < 3 {
-            Err("需要至少 3 个参数")
-        } else {
-            Ok(Config {
-                query: args[1].clone(),
-                path: args[2].clone(),
-                ignore_case: env::var("ignore_case").is_ok(),
-            })
-        }
-    }
+pub struct Map {
+    ground: Vec<Vec<char>>,
 }
-
-pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
-    let content = fs::read_to_string(&config.path)?;
-    if config.ignore_case {
-        for line in search_case_insensitive(&config.query, &content) {
-            println!("{line}");
-        }
-    } else {
-        for line in search_case_sensitive(&config.query, &content) {
-            println!("{line}");
-        }
+// private
+impl Map {}
+// public
+impl Map {
+    pub fn new(length: usize, width: usize) -> Self {
+        let ground = vec![vec![' '; length]; width];
+        Map { ground }
     }
-    Ok(())
-}
-pub fn search_case_sensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
-    let mut result = Vec::new();
-    for line in content.lines() {
-        if line.contains(query) {
-            result.push(line);
-        }
-    }
-    result
-}
-pub fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
-    let query = &query.to_lowercase();
-    let mut result = Vec::new();
-    for line in content.lines() {
-        if line.to_lowercase().contains(query) {
-            result.push(line);
-        }
-    }
-    result
 }
 #[cfg(test)]
 mod test {
     use super::*;
-
-    #[test]
-    fn case_insensitive() {
-        let query = "oh";
-        let content = "\
-你好！
-Hello, thank you!
-Oh, hello!
-床前明月光，疑是地上霜。";
-        assert_eq!(vec!["Oh, hello!"], search_case_insensitive(query, content));
+    mod private {
+        use super::*;
     }
-    #[test]
-    fn case_sensitive() {
-        let query = "床前明月光，";
-        let content = "\
-你好！
-Hello, thank you!
-Oh, hello!
-床前明月光，疑是地上霜。";
-        assert_eq!(
-            vec!["床前明月光，疑是地上霜。"],
-            search_case_sensitive(query, content)
-        );
-        let query = "Hello";
-        assert_eq!(
-            vec!["Hello, thank you!"],
-            search_case_sensitive(query, content)
-        );
+    mod public {
+        use super::*;
+        #[test]
+        fn new() {
+            let map = Map::new(3, 2);
+            let expect = vec![vec![' ', ' ', ' ']; 2];
+            /*
+            [[x,x,x],
+            [x,x,x]]
+             */
+            assert_eq!(map.ground, expect);
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,5 @@
-#![allow(non_camel_case_types)]
-use project::{run, Config};
-use std::{env, process};
+//use std::{env, process};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args);
-    if let Err(e) = run(&config) {
-        eprintln!("文件错误: {e}");
-        process::exit(1);
-    };
-}
-fn first_word(s: &str) -> &str {
-    let bytes = s.as_bytes();
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[0..i];
-        }
-    }
-    s
+    let a = game::Map::new(10, 5);
 }
