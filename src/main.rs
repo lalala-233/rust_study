fdeb94b2766d051fab22d96cde1daca06f1c534c
lalala-233 <2317987274@qq.com>
2023-07-28 23:43:46 +0800

    feat: add bevy

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,30 @@
-fn main() {}
+use std::env;
+use bevy::prelude::*;
+
+fn main() {
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            title: "Mine Sweeper!".to_string(),
+            resolution: (1920., 1080.).into(),
+            ..default()
+        }),
+        ..default()
+    }))
+    .add_systems(Startup, camera_setup);
+    if_debug(&mut app);
+    app.run();
+}
+
+fn camera_setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+fn if_debug(app: &mut App) {
+    let mut args = env::args();
+    args.next();
+    if args.next() == Some("debug".to_string()) {
+        use bevy_inspector_egui::quick::WorldInspectorPlugin;
+        app.add_plugins(WorldInspectorPlugin::new());
+    }
+}
